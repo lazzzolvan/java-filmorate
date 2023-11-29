ba8ceac18e981258e06d21f4fa5a121a5dd34c62
lazzzolvan <mr.mersen@yandex.ru>
2023-11-29 13:22:31 +0300

    сделал лайки

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,14 +2,20 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.DataException;
+import ru.yandex.practicum.filmorate.exception.DataNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.GenreStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
+import ru.yandex.practicum.filmorate.storage.memory.FilmDbStorage;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,15 +25,15 @@ import java.util.stream.Collectors;
 public class FilmService {
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
-    private final GenreStorage genreStorage;
+    private final JdbcTemplate jdbcTemplate;
     private static final LocalDate START_RELEASE_DATE = LocalDate.of(1895, 12, 28);
 
 
     @Autowired
-    public FilmService(@Qualifier("filmDbStorage") FilmStorage filmStorage, @Qualifier("userDbStorage") UserStorage userStorage, GenreStorage genreStorage) {
+    public FilmService(@Qualifier("filmDbStorage") FilmStorage filmStorage, @Qualifier("userDbStorage") UserStorage userStorage, JdbcTemplate jdbcTemplate) {
         this.filmStorage = filmStorage;
         this.userStorage = userStorage;
-        this.genreStorage = genreStorage;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public Film create(Film film) {
@@ -55,25 +61,57 @@ public class FilmService {
     public boolean addLikeFilm(Long filmID, Long userId) {
         userStorage.get(userId);
         filmStorage.get(filmID).addLike(userId);
+        String sqlQuery = "insert into LIKES(user_id, film_id)\n" +
+                "values(?, ?)";
+        int update = jdbcTemplate.update(sqlQuery, userId, filmID);
+        if (update == 0)
+            throw new DataNotFoundException(String.format("user with id %s or film with id %s not found", userId, filmID));
         return true;
     }
 
     public boolean removeLikeFilm(Long filmId, Long userId) {
         userStorage.get(userId);
-        filmStorage.get(filmId).getUsersByLike().remove(userId);
+        filmStorage.get(filmId);
+        String sqlQuery = "delete from LIKES where user_id = ? and film_id = ?";
+        int update = jdbcTemplate.update(sqlQuery, userId, filmId);
+        if (update == 0)
+            throw new DataNotFoundException(String.format("user with id %s or film with id %s not found", userId, filmId));
         return true;
     }
 
     public List<Film> getFirstCountFilms(Long count) {
-        return filmStorage.getAll().stream()
+        String sqlQuery = "SELECT count(film_id), FILM_ID \n" +
+                "FROM LIKES \n" +
+                "GROUP BY FILM_ID \n" +
+                "ORDER BY count(FILM_ID) DESC\n" +
+                "LIMIT ?;";
+        List<Integer> topFilmsId = jdbcTemplate.query(sqlQuery, this::findPopularFilms, count);
+        List<Film> films = new ArrayList<>();
+        if (topFilmsId.size() == 0) {
+            String sqlQueryFilms = "select id as film_id from FILMS limit ?";
+            List<Integer> filmIds = jdbcTemplate.query(sqlQueryFilms, this::findPopularFilms, count);
+            for (Integer filmId : filmIds) {
+                films.add(get(Long.valueOf(filmId)));
+            }
+            return films;
+        }
+        for (Integer currentId : topFilmsId) {
+            films.add(get(Long.valueOf(currentId)));
+        }
+        return films;
+/*        return filmStorage.getAll().stream()
                 .sorted(Comparator.comparingInt(film -> -1 * film.getUsersByLike().size()))
                 .limit(count)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList());*/
     }
 
     public void validate(Film data) {
         if (data.getReleaseDate().isBefore(START_RELEASE_DATE)) {
             throw new ValidationException("Film release data is invalid");
         }
+    }
+
+    int findPopularFilms(ResultSet rs, int rowNum) throws SQLException {
+        return rs.getInt("film_id");
     }
 }
